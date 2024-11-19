fac4d014ccf48f5781966fafe987481ea39fb032
Daniel Araneda M <d.aranedam@duocuc.com>
2024-11-19 14:49:48 -0300

    reportesQA

---
+++ src/test/java/Ejecutador/Runner.java
@@ -0,0 +1,16 @@
+package Ejecutador;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src/test/resources/feature"},
+				glue= {"DefinicionPasos"},
+				plugin = {"json:target/cucumber.json","pretty","html:target/cucumber-reports/report.html", "junit:target/cucumber-results.xml"}, 
+				monochrome = true)
+
+
+public class Runner {
+
+}
